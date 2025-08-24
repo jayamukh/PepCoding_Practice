bad8f4d798a2b6dd75ddc347bb1f005787ec733d
Jaya Mukherjee <jayamukherjee@Mac.attlocal.net>
2025-08-23 21:37:03 -0700

    Adding test code for calculating account balances

---
+++ src/main/java/org/example/AccountBalance.java
@@ -0,0 +1,67 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AccountBalance {
+
+   public static int[] Calcualte(int[] balances, String[] requests)
+   {
+      // String[] cashback = new String[]{};
+       ArrayList<String> cashback = new ArrayList<>();
+       int count = 0;
+       int read = 0, write = 0;
+       for(String s: requests)
+       {
+           count++;
+           String func = s.split(" ")[0];
+           long timestp = Long.parseLong(s.split(" ")[1]);
+           int acc = Integer.parseInt(s.split(" ")[2]);
+           int amt = Integer.parseInt(s.split(" ")[3]);
+
+           if(!cashback.isEmpty())
+           {
+              String str =   cashback.getFirst();
+               long timestp1 = Long.parseLong(str.split(" ")[1]);
+               int acc1 = Integer.parseInt(str.split(" ")[2]);
+               int amt1 = Integer.parseInt(str.split(" ")[3]);
+               if(timestp1 < timestp)
+               {
+                   balances[acc1-1] += amt1;
+                   cashback.removeFirst();
+               }
+           }
+           if(func.equals("withdraw"))
+           {
+               balances[acc-1] -= amt;
+               if(balances[acc-1] < 0)
+               {
+                   return new int[]{-count};
+               }
+               cashback.add( "withdraw " + (timestp + (3600 * 24)) +" "+ acc+" " + ((int)(amt * .02))+" ");
+           }
+           else if(func.equals("deposit"))
+           {
+               balances[acc-1] += amt;
+           }
+       }
+
+
+       return balances;
+   }
+
+    public static void main(String[] args)
+    {
+        int[] balances = new int[]{1000, 1500};
+        String[] requests = new String[]{
+        "withdraw 1613327630 2 480",
+                "withdraw 1613327644 2 800",
+                "withdraw 1614105244 1 100",
+                "deposit 1614108844 2 200",
+                "withdraw 1614108845 2 150"};
+
+        int[] res = Calcualte(balances, requests);
+
+        System.out.println(Arrays.toString(res));
+    }
+}
